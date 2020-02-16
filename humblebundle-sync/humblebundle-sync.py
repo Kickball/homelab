0048c965b82df98b664d14c9e5c89446759d1a09
Edward Dickson <ed.dickson@hotmail.com>
2020-02-16 19:59:23 +0000

    HBD - shell not python function and copy cookie

--- humblebundle-sync/humblebundle-sync.py
+++ humblebundle-sync/humblebundle-sync.py
@@ -1,30 +1,25 @@
-#https://stackoverflow.com/a/11447648/3085144
-import os
-#https://github.com/xtream1101/humblebundle-downloader/
-import humblebundle-downloader as hbd
-
-#https://stackoverflow.com/a/14981125/3085144
 from __future__ import print_function
 import sys
+import os
+import subprocess
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)from __future__ import print_function
-import sys
-
+##https://stackoverflow.com/a/14981125/3085144
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 #https://stackoverflow.com/a/11447648/3085144
-if !('HBD_LIBRARY_PATH' in os.environ):
+if ('HBD_LIBRARY_PATH' not in os.environ):
    eprint("Error: HBD_LIBRARY_PATH Environment Variable not set")
    quit()
 else:
    hbdLibraryPath = os.environ.get('HBD_LIBRARY_PATH')
 
-if !('HBD_COOKIE' in os.environ):
+if ('HBD_COOKIE' not in os.environ):
    eprint("Error: HBD_COOKIE Environment Variable not set")
    quit()
 else:
    hbdCookie = os.environ.get('HBD_COOKIE')
 
-hbd download --cookie-file hbdCookie --library-path hbdLibraryPath --update
+#https://github.com/xtream1101/humblebundle-downloader/
+#https://stackoverflow.com/a/40319875/3085144
+subprocess.run(["hbd","download","--cookie-file",hbdCookie,"--library-path",hbdLibraryPath,"--update"], stdout=subprocess.PIPE)
