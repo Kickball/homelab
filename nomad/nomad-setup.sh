f785ce64c1d373cdc2eeb5ab8de6ddae2f7681c4
Edward Dickson <ed.dickson@hotmail.com>
2020-02-23 14:27:25 +0000

    Nomad setup

---
+++ nomad/nomad-setup.sh
@@ -0,0 +1,4 @@
+wget https://releases.hashicorp.com/nomad/0.10.4/nomad_0.10.4_linux_arm.zip
+unzip nomad_0.10.4_linux_arm.zip
+rm nomad_0.10.4_linux_arm.zip
+sudo ln -s /home/edward/homelab/nomad/nomad /usr/bin/
