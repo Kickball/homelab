79f51b45772345da1eeceab2a9bd4aa4c9394135
Edward Dickson <ed.dickson@hotmail.com>
2020-04-18 23:25:16 +0100

    Improve nomad setup script for updating nomad

--- nomad/nomad-setup.sh
+++ nomad/nomad-setup.sh
@@ -1,4 +1,6 @@
-wget https://releases.hashicorp.com/nomad/0.10.4/nomad_0.10.4_linux_arm.zip
-unzip nomad_0.10.4_linux_arm.zip
-rm nomad_0.10.4_linux_arm.zip
-sudo ln -s /home/edward/homelab/nomad/nomad /usr/bin/
+wget https://releases.hashicorp.com/nomad/0.11.0/nomad_0.11.0_linux_arm.zip
+unzip -u nomad_0.11.0_linux_arm.zip
+rm nomad_0.11.0_linux_arm.zip
+if [ ! -L '/usr/bin/nomad' ]; then
+  sudo ln -s $PWD/nomad /usr/bin/
+fi
