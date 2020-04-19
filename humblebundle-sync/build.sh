5933462c326a7f8c6ea89fd567dd5a9c6ddc167c
Edward Dickson <ed.dickson@hotmail.com>
2020-04-19 21:55:04 +0100

    Move docker tagging from push to build script

--- humblebundle-sync/build.sh
+++ humblebundle-sync/build.sh
@@ -1,1 +1,2 @@
 docker build -t humblebundle-sync .
+docker tag humblebundle-sync:latest 192.168.1.178:5000/humblebundle-sync
--- humblebundle-sync/push.sh
+++ humblebundle-sync/push.sh
@@ -1,2 +1,1 @@
-docker tag humblebundle-sync:latest 192.168.1.178:5000/humblebundle-sync
 docker push 192.168.1.178:5000/humblebundle-sync
