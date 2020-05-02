725582a9ab0592958dfc8344bb9e3f4e94dc56a6
Edward Dickson <ed.dickson@hotmail.com>
2020-05-02 01:07:54 +0100

    Update vault to 1.4.1

--- vault/vault-setup.sh
+++ vault/vault-setup.sh
@@ -1,6 +1,6 @@
-wget https://releases.hashicorp.com/vault/1.4.0/vault_1.4.0_linux_arm.zip
-unzip -u vault_1.4.0_linux_arm.zip
-rm vault_1.4.0_linux_arm.zip
+wget https://releases.hashicorp.com/vault/1.4.1/vault_1.4.1_linux_arm.zip
+unzip -u vault_1.4.1_linux_arm.zip
+rm vault_1.4.1_linux_arm.zip
 if [ ! -L '/usr/bin/vault' ]; then
   sudo ln -s $PWD/vault /usr/bin/
 fi
