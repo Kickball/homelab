d8c38a95ef214213a8ee0fdd64570778530ddd65
Edward Dickson <ed.dickson@hotmail.com>
2020-05-23 13:27:06 +0100

    Hashicorp scripting improvements

--- nomad/nomad-setup.sh
+++ nomad/nomad-setup.sh
@@ -1,27 +1,40 @@
 VERSION=0.11.2
 
 #Detect host type (e.g. sm [server master] = nomad server, ss [server slave] = nomad server & nomad client, sw [server worker] = nomad client)
-HOST_TYPE= /bin/echo $HOSTNAME | grep -oP "[A-Za-z]{2}(?=[0-9])+"
+HOST_TYPE=$(echo $HOSTNAME | grep -oP "[A-Za-z]{2}(?=[0-9])+")
+
+function download_and_install {
+  # Download and "install" the requested version.
+  /usr/bin/wget https://releases.hashicorp.com/nomad/${VERSION}/nomad_${VERSION}_linux_arm.zip
+  /usr/bin/unzip -uo nomad_{VERSION}_linux_arm.zip
+  /bin/rm nomad_{VERSION}_linux_arm.zip
+
+  # Create symlink to a directory on the PATH if doesn't exist.
+  if [ ! -L '/usr/bin/nomad' ]; then
+    sudo ln -s $PWD/nomad /usr/bin/
+  fi
+}
 
 # Check if nomad is already setup as a service
-if [${HOST_TYPE} = "sm"]; then
-  if [service --status-all | grep -Fq 'nomad']; then
-    # Stop nomad services, if they exists.
-    if [service --status-all | grep -Fq 'nomad-client']; then
-      sudo service nomad-client stop
-    fi
-    if [service --status-all | grep -Fq 'nomad-server]'; then
-      sudo service nomad-server stop
-    fi
+if [ "${HOST_TYPE}" = "sm" ]; then
+  echo "sm detected"
+  # Stop nomad services, if they exists.
+  if [ systemctl status 2>/dev/null | grep -Fq 'nomad-server' ]; then
+    sudo systemctl stop nomad-server
   fi
-fi
-
-# Download and "install" the requested version.
-wget https://releases.hashicorp.com/nomad/${VERSION}/nomad_${VERSION}_linux_arm.zip
-unzip -uo nomad_{VERSION}_linux_arm.zip
-rm nomad_{VERSION}_linux_arm.zip
-
-# Create symlink to a directory on the PATH if doesn't exist.
-if [ ! -L '/usr/bin/nomad' ]; then
-  sudo ln -s $PWD/nomad /usr/bin/
+elif [ "${HOST_TYPE}" = "ss" ]; then
+  echo "ss detected"
+  # Stop nomad services, if they exists.
+  if [ systemctl status 2>/dev/null | grep -Fq 'nomad-server ]'; then
+    sudo systemctl stop nomad-server
+  fi
+  if [ systemctl status 2>/dev/null | grep -Fq 'nomad-client' ]; then
+    sudo systemctl stop nomad-server
+  fi
+elif [ "${HOST_TYPE}" = "sw" ]; then
+  echo "sw detected"
+  # Stop nomad services, if they exists.
+  if [ systemctl status 2>/dev/null | grep -Fq 'nomad-client' ]; then
+    sudo systemctl stop nomad-server
+  fi
 fi
