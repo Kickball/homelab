dc0f7516fd8b853897210592aa803ce572125df1
Edward Dickson <ed.dickson@hotmail.com>
2020-05-26 21:19:41 +0100

    Consul install script creation

--- consul/consul-setup.sh
+++ consul/consul-setup.sh
@@ -1,12 +1,82 @@
+#!/bin/bash
+
+if [[ $EUID > 0 ]]; then
+  echo "Please run as root"
+  exit 1
+fi
+
 VERSION="1.7.3"
 
-echo ${VERSION}
+#Detect host type (e.g. sm [server master] & ss [server slave] = consul server & consul client, sw [server worker] = consul client)
+HOST_TYPE=$(echo $HOSTNAME | grep -oP "[A-Za-z]{2}(?=[0-9])+")
 
-wget https://releases.hashicorp.com/consul/${VERSION}/consul_${VERSION}_linux_armhfv6.zip
-unzip -uo consul_${VERSION}_linux_armhfv6.zip
-rm consul_${VERSION}_linux_armhfv6.zip
-if [ ! -L '/usr/bin/consul' ]; then
-  sudo ln -s $PWD/consul /usr/bin/
+download_and_install_consul () {
+  # Download and "install" the requested version.
+  wget https://releases.hashicorp.com/consul/${VERSION}/consul_${VERSION}_linux_armhfv6.zip
+  unzip -uo consul_${VERSION}_linux_armhfv6.zip
+  rm consul_${VERSION}_linux_armhfv6.zip
+  if [ ! -L '/usr/bin/consul' ]; then
+   sudo ln -s $PWD/consul /usr/bin/
+  fi
+  consul -autocomplete-install
+  complete -C /usr/bin/consul consul
+}
+
+add_consul_config_file () {
+  if [ ! -d /etc/consul-$1.d ]; then
+    mkdir /etc/consul-$1.d
+  fi
+  if [ ! -f /etc/consul-$1.d/consul-$1.hcl ]; then
+    cp consul-$1.hcl /etc/consul-$1.d/consul-$1.hcl
+  fi
+}
+
+add_consul_systemd_entry () {
+  if [ ! -f /etc/systemd/system/consul-$1.service ]; then
+    cp consul-$1.service /etc/systemd/system/consul-$1.service
+  fi
+}
+
+# Check if consul is already setup as a service
+if [ "${HOST_TYPE}" = "sm" ] || [ "${HOST_TYPE}" = "ss" ]; then
+  echo "ss or sm detected"
+  # Stop consul services, if they exists.
+  if [ -f /etc/systemd/system/consul-server.service ]; then
+    systemctl stop consul-server
+  fi
+  if [ -f /etc/systemd/system/consul-client.service ]; then
+    systemctl stop consul-client
+  fi
+  #Download and 'install' the consul binary
+  download_and_install_consul
+  
+  # Setup consul client
+  add_consul_config_file client
+  add_consul_systemd_entry client
+
+  #Setup consul server
+  add_consul_config_file server
+  add_consul_systemd_entry server
+
+  # Enable and Start consul Services
+  systemctl enable consul-client
+  systemctl start consul-client
+  systemctl enable consul-server
+  systemctl start consul-server
+elif [ "${HOST_TYPE}" = "sw" ]; then
+  echo "sw detected"
+  # Stop consul services, if they exists.
+  if [ -f /etc/systemd/system/consul-client.service ]; then
+    systemctl stop consul-client
+  fi
+  #Download and 'install' the consul binary
+  download_and_install_consul
+
+  # Setup consul client
+  add_consul_config_file client
+  add_consul_systemd_entry client
+
+  # Enable and Start consul Services
+  systemctl enable consul-client
+  systemctl start consul-client
 fi
-consul -autocomplete-install
-complete -C /usr/local/bin/consul consul
--- vault/vault-version-check.sh
+++ vault/vault-version-check.sh
@@ -1,4 +1,4 @@
 NEW_VERSION=$(curl --stderr - https://www.vaultproject.io/downloads  | grep -o 'Current Version:</strong> <!-- -->[0-9]*\.[0-9]*\.[0-9]*' | grep -o "[0-9]*\.[0-9]*\.[0-9]*" | uniq)
 CURRENT_VERSION=$(vault version | grep -o "[0-9]*\.[0-9]*\.[0-9]*" )
-echo ${NEW_VERSION}
-echo ${CURRENT_VERSION}
+echo "New version: ${NEW_VERSION}"
+echo "Current version: ${CURRENT_VERSION}"
