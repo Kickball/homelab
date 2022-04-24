903c20e5b5e669ebdb43ccc29af3be1c73c8bd95
Edward <ed.dickson@hotmail.com>
2022-04-24 18:34:35 +0000

    Update hashicorp update scripts to better match website HTML

--- nomad/nomad-version-check.sh
+++ nomad/nomad-version-check.sh
@@ -1,4 +1,4 @@
-NEW_VERSION=$(curl --stderr - https://www.nomadproject.io/downloads  | grep -o '<span class="style_version__2u5ed">[0-9]*\.[0-9]*\.[0-9]*' | grep -o "[0-9]*\.[0-9]*\.[0-9]*" | uniq)
+NEW_VERSION=$(curl --stderr - https://www.nomadproject.io/downloads  | grep -o '<span class="style_version__[a-zA-Z0-9]*">[0-9]*\.[0-9]*\.[0-9]*' | grep -o "[0-9]*\.[0-9]*\.[0-9]*" | uniq)
 CURRENT_VERSION=$(nomad version | grep -o "[0-9]*\.[0-9]*\.[0-9]*")
 echo "New version: ${NEW_VERSION}"
 echo "Current version: ${CURRENT_VERSION}"
--- vault/vault-version-check.sh
+++ vault/vault-version-check.sh
@@ -1,4 +1,4 @@
-NEW_VERSION=$(curl --stderr - https://www.vaultproject.io/downloads  | grep -o 'Current Version:</strong> <!-- -->[0-9]*\.[0-9]*\.[0-9]*' | grep -o "[0-9]*\.[0-9]*\.[0-9]*" | uniq)
+NEW_VERSION=$(curl --stderr - https://www.vaultproject.io/downloads  | grep -o '<span class="style_version__[a-zA-Z0-9]*">[0-9]*\.[0-9]*\.[0-9]*' | grep -o "[0-9]*\.[0-9]*\.[0-9]*" | uniq)
 CURRENT_VERSION=$(vault version | grep -o "[0-9]*\.[0-9]*\.[0-9]*" )
 echo "New version: ${NEW_VERSION}"
 echo "Current version: ${CURRENT_VERSION}"
