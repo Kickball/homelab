c5cf20252dd1b4c3058f5621467a93c92755474d
Edward <ed.dickson@hotmail.com>
2022-08-17 17:38:25 +0000

    Update smokeping build to use variables

--- smokeping/build.sh
+++ smokeping/build.sh
@@ -1,2 +1,4 @@
+IP=$(ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet" | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1)
+PORT=5000
 podman build -t smokeping . --build-arg SMOKEPING_VERSION=$1
-podman tag smokeping:latest 192.168.20.132:5000/smokeping
+podman tag smokeping:latest $IP:$PORT/smokeping
